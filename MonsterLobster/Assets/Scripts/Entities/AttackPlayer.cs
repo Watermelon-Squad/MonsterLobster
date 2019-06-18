fb04857dae6d2d909abde12b66540d9a42907b21
Siitoo <alfonsosanchez1896@gmail.com>
2019-06-18 12:34:32 +0200

    Add attack player collider script

---
+++ MonsterLobster/Assets/Scripts/Entities/AttackPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackPlayer : MonoBehaviour
+{
+    public float offset_x = 1.0f;
+    public float offset_y = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 new_position = new Vector3(Input.GetAxis("Horizontal") * offset_x, Input.GetAxis("Vertical")* offset_y, 0);
+
+        transform.position = gameObject.transform.parent.position + new_position;
+    }
+}
