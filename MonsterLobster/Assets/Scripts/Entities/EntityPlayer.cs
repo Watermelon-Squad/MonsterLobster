aef5c85e3c06bd71fac5ea541fdb4a8e4acd2645
Siitoo <alfonsosanchez1896@gmail.com>
2019-06-18 11:54:45 +0200

    Player movement

---
+++ MonsterLobster/Assets/Scripts/Entities/EntityPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityPlayer : MonoBehaviour
+{
+    EntityPlayer Call = null;
+
+    public float velocity = 1.0f;
+    public float dash_velocity = 3.0f;
+    public float dash_cooldown = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Call = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 new_position = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * (velocity * Time.deltaTime);
+        gameObject.transform.Translate(new_position);
+
+    }
+}
