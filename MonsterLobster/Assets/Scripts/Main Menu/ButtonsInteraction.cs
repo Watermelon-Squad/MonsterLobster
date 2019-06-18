e6cdd9a92f225f1ce4f69abcc1b2fbd8c4870de4
Siitoo <alfonsosanchez1896@gmail.com>
2019-06-18 17:09:19 +0200

    Main Scene

---
+++ MonsterLobster/Assets/Scripts/Main Menu/ButtonsInteraction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsInteraction : MonoBehaviour
+{
+    public void onClickPlay()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    public void onClickQuit()
+    {
+        Application.Quit();
+    }
+}
