d5e753c9d47cc18c52b99bb6aa29c1dd826b26de
Siitoo <alfonsosanchez1896@gmail.com>
2019-06-19 04:54:59 +0200

    Scripts create terrain

---
+++ MonsterLobster/Assets/Scripts/Map/TerrainGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    private GameObject go = null;
+
+    public bool is_left = true;
+    public GameObject[] gameObjects;
+    
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            if(go == null)
+            {
+                int r = Random.Range(0, gameObjects.Length - 1);
+                Vector3 new_position = Vector3.zero;
+
+                if(is_left)
+                {
+                    new_position = new Vector3(transform.parent.parent.position.x - 28.7f, transform.parent.parent.position.y, transform.parent.parent.position.z);
+                }
+                else
+                {
+                    new_position = new Vector3(transform.parent.parent.position.x + 28.7f, transform.parent.parent.position.y, transform.parent.parent.position.z);
+                }
+
+                go = GameObject.Instantiate(gameObjects[r],new_position, transform.parent.parent.rotation);
+            }
+        }
+    }
+
+}
