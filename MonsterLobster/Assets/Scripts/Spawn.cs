2cf2e647120bb2c1d4fc1274d1bb67aca21ecebd
Siitoo <alfonsosanchez1896@gmail.com>
2019-06-19 07:25:07 +0200

    spawn scripts

---
+++ MonsterLobster/Assets/Scripts/Spawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject[] go;
+
+    private void Start()
+    {
+        if (go.Length - 1 < 0)
+        {
+            int r = Random.Range(0, go.Length - 1);
+
+            GameObject.Instantiate(go[r],transform);
+        }
+    }
+    
+}
