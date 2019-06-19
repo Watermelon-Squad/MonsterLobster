1c8539edeb93e612664a440b818bcaaedf364a06
Sersius <segoro97@msn.com>
2019-06-19 11:26:57 +0200

    Practically all sounds addded, and its triggered colliders

--- MonsterLobster/Assets/Scripts/Entities/DeadEnemy.cs
+++ MonsterLobster/Assets/Scripts/Entities/DeadEnemy.cs
@@ -9,6 +9,7 @@ public class DeadEnemy : MonoBehaviour
     public bool death = false;
     int once = 0;
     public GameObject[] blood;
+    public AudioClip[] audios_enemy;
 
     // Update is called once per frame
 
@@ -21,10 +22,10 @@ public class DeadEnemy : MonoBehaviour
     {
         if(death)
         {
+           
             animator.SetBool("attack", false);
             animator.SetBool("death", true);
             BloodStain();
-
         }
     }
 
@@ -32,6 +33,9 @@ public class DeadEnemy : MonoBehaviour
     {
         if (once == 0)
         {
+            AudioSource audiosource = gameObject.GetComponent<AudioSource>();
+            audiosource.PlayOneShot(audios_enemy[0]);
+            audiosource.PlayOneShot(audios_enemy[1]);
             transform.GetComponent<BoxCollider2D>().enabled = false;
             GameObject.Instantiate(blood[Random.RandomRange(0, 2)], transform.position, transform.rotation);
             once++;
