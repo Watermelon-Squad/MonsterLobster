63e0c3a591fd6db9c78d3970015432d68b1459d6
DavidTheMaaster <davidthegaamer@gmail.com>
2019-06-19 06:22:13 +0200

    Added Story Scene

---
+++ MonsterLobster/Assets/Scripts/UI Interactions/Story.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Story : MonoBehaviour
+{
+    public Image Story_2;
+    public Image Story_3;
+    public Image Story_4;
+    public Image Story_5;
+
+    float timer = 2.0f;
+    int actualphoto = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer<=0.0f)
+        {
+            if(actualphoto == 1)
+            {
+                transform.Translate(new Vector3(1000.0f, 0.0f, 0.0f));
+            }
+            if (actualphoto == 2)
+            {
+                Story_2.gameObject.active = false;
+            }
+            if (actualphoto == 3)
+            {
+                Story_3.gameObject.active = false;
+            }
+            if (actualphoto == 4)
+            {
+                Story_4.gameObject.active = false;     
+            }
+            if (actualphoto == 5)
+            {
+                SceneManager.LoadScene("Main Scene");
+            }
+
+            timer = 2.0f;
+            actualphoto++;
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+}
